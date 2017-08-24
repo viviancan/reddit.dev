da23d47f01e3c246f2b7e36db7ce122531534527
Vivian <vcnales@gmail.com>
2017-08-24 16:22:26 -0500

    final commit before walkthrough. laravel migrations

---
+++ database/migrations/2017_08_24_204731_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->string('table');
+            
+            $table->string('url');
+
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
+
