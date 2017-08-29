e53fb8c271141e957f9aaa783be22cb438e5f849
Vivian <vcnales@gmail.com>
2017-08-29 13:38:20 -0500

    used faker library to seed. TODO: Fix truncate issue

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+    	DB::table('posts')->truncate();
+
+    	$posts = [];
+
+    	$faker = Faker\Factory::create();
+
+    	for ($i = 1; $i <= 10; $i++)
+    	{
+    		$posts[] = [
+
+    			'title'=> $faker->sentence($nbWords = 6, $variableNbWords = true),
+    			'url'=> $faker->url,
+    			'content'=> $faker->text,
+    			'created_by'=>rand(0,10),
+    			'created_at'=> $faker->dateTime(),
+    			'updated_at'=> $faker->dateTime()
+    		];
+    	}
+
+    	DB::table('posts')->insert($posts);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+// require_once '/vendor/fzaninotto/Faker/src/autoload.php';
+
+use Faker\Factory;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	// DB::statement('SET FOREIGN_KEY_CHECKS=0');
+    	DB::table('users')->truncate();
+
+    	$users = [];
+
+    	$faker = Faker\Factory::create();
+
+    	for ($i = 1; $i <= 10; $i++)
+    	{
+    		$users[] = [
+
+    			'name'=> $faker->name,
+    			'email'=> $faker->email,
+    			'password'=> Hash::make('password123'),
+    			'created_at'=> $faker->dateTime(),
+    			'updated_at'=> $faker->dateTime()
+    		];
+    	}
+
+    	DB::table('users')->insert($users);
+
+    }
+}
